e40331132b7389178f6f49d97f8942b45f014843
MARCZERPC <Oula@divingrace.com>
2025-10-20 11:03:17 +0000

    test connect github

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -18,9 +18,9 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetMember(string id)
+        public async Task<ActionResult<AppUser>> GetMember(string id)
         {
-            var member = context.Users.Find(id);
+            var member = await context.Users.FindAsync(id);
             if (member == null) return NotFound();
             return Ok(member);
         }
